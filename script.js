3c5ad212f95e43b19435ed88e2c4ca15def9793a
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-03-10 21:02:30 -0300

    Dia 4 Mobile First

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ toggleMenu.addEventListener('click',()=>{
 closeMenu.addEventListener('click',()=>{
     navMenu.classList.remove('show')
 })
+
+/*--------REMOVE MENU----------*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*---------SCROLL SECTIONS ACTIVE LINK--------*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight){
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active')
+        }
+    })
+}
